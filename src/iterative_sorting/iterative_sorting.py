3fbf3dfeb2b6118cbdbc61213fd988af16934b01
IsabellaGuo <isabella.guofei@gmail.com>
2020-05-18 23:17:24 -0700

    finished mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -3,14 +3,23 @@ def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
+        # Find the smallest number 
+        # move the smallest number to the head
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
+        
+        for j in range (cur_index + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
+        
 
         # TO-DO: swap
         # Your code here
+
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
     return arr
 
@@ -18,9 +27,19 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    # compare first two elements, swap if second is smalled than the first
+    # compare the second and the next one, swap if the next is smaller than the first
+    for i in range (len(arr)-1):
+        for j in range (len(arr)-1-i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
     return arr
+
+
+
+
+    
 
 
 # STRETCH: implement the Count Sort function below
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,9 @@
 def linear_search(arr, target):
     # Your code here
-
+    # if head value is the target
+    for i in range(len(arr)):
+        if target == arr[i]:
+            return i
 
     return -1   # not found
 
@@ -9,6 +12,25 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    # set low and high
+    low = 0
+    high = len(arr) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        # if midpoint is the target
+        if arr[mid] == target:
+            return mid
+
+        if arr[mid] < target:
+            low = mid + 1
+        if arr[mid] > target:
+            high = mid - 1
+
+        
+
+
 
 
     return -1  # not found
+ 
